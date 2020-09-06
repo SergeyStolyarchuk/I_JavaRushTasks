430f04dd1daddff28dad7bfffe8a893635f49834
Sett <Sett_tm@bk.ru>
2020-09-06 15:21:29 +0300

    60920

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task16.task1630;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 /* 
 Последовательный вывод файлов
@@ -14,6 +12,18 @@ public class Solution {
     public static String secondFileName;
 
     //add your code here - добавьте код тут
+    static {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public static void main(String[] args) throws InterruptedException {
         systemOutPrintln(firstFileName);
@@ -23,8 +33,10 @@ public class Solution {
     public static void systemOutPrintln(String fileName) throws InterruptedException {
         ReadFileInterface f = new ReadFileThread();
         f.setFileName(fileName);
+
         f.start();
         //add your code here - добавьте код тут
+        f.join();
         System.out.println(f.getFileContent());
     }
 
@@ -40,4 +52,37 @@ public class Solution {
     }
 
     //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        String nameFileToRead;
+        String fileContent = "";
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.nameFileToRead = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            return fileContent;
+        }
+
+        @Override
+        public void run() {
+            File file = new File(nameFileToRead);
+
+            try {
+                Scanner sc = new Scanner(file);
+                while (sc.hasNextLine()) {
+                 fileContent += sc.next() + " ";
+
+                }
+                fileContent = fileContent.trim();
+                sc.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
